e5e34e5b320aaeac42d7936387529da28f3fb524
付圣 <ysudqfs@163.com>
2018-09-06 20:18:28 +0800

    类型转换

--- hello/change_type.go
+++ hello/change_type.go
@@ -5,11 +5,20 @@ import "fmt"
 //  数据类型的转换
 
 func main() {
-
+	// 不能转换的类型，叫做不兼容类型
 	var flag bool
 	flag = true
-	fmt.Printf("flag=%d\n", flag) // %d 匹配整形
+	//	fmt.Printf("flag=%d\n", flag) // %d 匹配整形
 	fmt.Printf("flag=%t\n", flag)
+	// bool类型不能转换为Int
+	// 整形也不能转换为bool
+	// flag=bool(1)
+	fmt.Println("flag = ", flag)
 
-	fmt.Printf("flag = ")
+	var ch byte
+	ch = 'a' // 字符类型本质就是整形
+	var t int
+	t = int(ch)
+	fmt.Println("t=", t)
+
 }
