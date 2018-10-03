f72900d8934e3a56f34cb20225cbfed5866d9e0d
付圣 <ysudqfs@163.com>
2018-10-03 17:03:59 +0800

    change func name

--- hello/review_go/by_go/02_insert_sort.go
+++ hello/review_go/by_go/02_insert_sort.go
@@ -4,22 +4,21 @@ import "fmt"
 
 func main() {
 	//插入排序
-	arr := []int{1,2,5,8,7,9,6,4,3,0}
-	fmt.Println(InertSort(arr))
+	arr := []int{1, 2, 5, 8, 7, 9, 6, 4, 3, 0}
+	fmt.Println(InsertSort(arr))
 }
 
-
-func InertSort(arr []int)[]int{
+func InsertSort(arr []int) []int {
 	num := len(arr)
-	for i:=1; i<num; i++{
+	for i := 1; i < num; i++ {
 		min := arr[i]
-		j := i-1
-		for j >=0 && arr[j] >= min{
+		j := i - 1
+		for j >= 0 && arr[j] >= min {
 			arr[j+1] = arr[j]
 			j--
 			arr[j+1] = min
 		}
 	}
 
- 	return arr
+	return arr
 }
